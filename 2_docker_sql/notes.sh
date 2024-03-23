33c16ffae9e5bb78fc82c3516cdeb4d4297d56df
Ivan <scorpione.ivan@gmail.com>
2024-03-23 12:01:22 +0400

    new

--- 2_docker_sql/notes.sh
+++ 2_docker_sql/notes.sh
@@ -31,12 +31,12 @@ python ingest_data.py \
 
 winpty docker build -t taxi_ingest:v001 .
 
-winpty docker run -it
+winpty docker run -it \
   --network=pg-network \
   taxi_ingest:v001 \
     --user=root \
     --password=root \
-    --host=localhost \
+    --host=pg-database \
     --port=5432 \
     --db=ny_taxi \
     --table_name=yellow_taxi_trips \
